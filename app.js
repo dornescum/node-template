463b186e1872a0c41150daa19abdee8ab2a03554
Mihai Dornescu <dornescu@gmail.com>
2021-08-30 09:49:58 +0300

    passing data ejs

--- app.js
+++ app.js
@@ -11,22 +11,27 @@ const port = 3000;
 
 app.listen(port);
 //listen for request || este importanta ordinea in care sunt aranjate
-app.get('/', (req,res)=>{
-    // este important obiectul (spune de unde porneste)
-    // res.sendFile('./views/index.ejs', {root: __dirname})
-    res.render('index')
+app.get('/', (req, res) => {
+	// este important obiectul (spune de unde porneste)
+	// res.sendFile('./views/index.ejs', {root: __dirname})
+	const dogsList = [
+		{title: 'Eva', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, sit.\n'},
+		{title: 'Doggy', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, sit.\n'},
+		{title: 'dogPose', desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius, sit.\n'},
+	]
+	res.render('index', {title: 'Home', dogs: dogsList})
 })
-app.get('/dogs', (req,res)=>{
-    // pt html sentFile
-    // res.sendFile('./views/dogs.ejs', {root: __dirname})
-    res.render('dogs');
+app.get('/dogs', (req, res) => {
+	// pt html sentFile
+	// res.sendFile('./views/dogs.ejs', {root: __dirname})
+	res.render('dogs', {title: 'All Dogs'});
 })
 // redirect
-app.get('/dogs-new', (req,res)=>{
-    res.redirect('/dogs');
+app.get('/dogs-new', (req, res) => {
+	res.redirect('/dogs');
 })
 // 404 && si trebuie pus ultimul dupa ce trece prin toate
-app.use((req,res)=>{
-    // res.sendFile('./views/404.ejs', {root: __dirname})
-    res.status(404).render('404');
+app.use((req, res) => {
+	// res.sendFile('./views/404.ejs', {root: __dirname})
+	res.status(404).render('404', {title: 'Error'});
 })
